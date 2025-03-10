501e4122e23b72a60f6bd96a870a15b11eed2736
Mark0930 <mark0930@gmail.com>
2025-03-10 09:24:33 +0000

    feat: connecting to postgres db from docker

--- src/database.py
+++ src/database.py
@@ -2,15 +2,17 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-#SQLALCHEMY_DATABASE_URL = "postgresql://postgres:mysecretpassword@db:5432/sampledb"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:mysecretpassword@db:5432/sampledb"
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:mysecretpassword@localhost:5432/sampledb"
+# SQLALCHEMY_DATABASE_URL = "postgresql://postgres:mysecretpassword@localhost:5432/sampledb"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+Base.metadata.create_all(bind=engine)
 
 def get_db():
     db = SessionLocal()
